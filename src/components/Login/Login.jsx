2bf89e4194e2b288d71f0dc177f191c8ffcad952
emoncse2020 <emon.cse.2020@gmail.com>
2025-06-01 02:24:14 +0600

    done for this time

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,22 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../Firebase/firebase.init";
+
 const Login = () => {
-  const handleClick = () => {
-    console.log("Clicked");
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        console.log(res.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
   return (
     <div>
-      <button onClick={handleClick}> Login with Google</button>
+      <button onClick={handleGoogleSignIn}> Login with Google</button>
     </div>
   );
 };
